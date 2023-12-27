0af4c61c2b67b721d673e244d9f2e36a21e14c5c
Robert Nor <quizzez1912@gmail.com>
2023-12-27 19:41:12 +0100

    added every file again and now working anim

---
+++ Assets/Scripts/Levelbar.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Levelbar : MonoBehaviour
+{
+    private Slider slider;
+    private float targetValue;
+    private float fillSpeed = 10;
+
+    private void Awake()
+    {
+        slider = gameObject.GetComponent<Slider>();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        addXP(20);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(slider.value < targetValue)
+        {
+            slider.value += fillSpeed * Time.deltaTime;
+
+        }   
+    }
+
+    public void addXP(float newXP)
+    {
+        targetValue = slider.value + newXP;
+    }
+
+
+}
