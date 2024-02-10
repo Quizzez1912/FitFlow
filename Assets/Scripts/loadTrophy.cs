d0b2f2a1c9f1350b51151cc0fb22133772a2d41b
Robert <quizzez1912@gmail.com>
2024-02-10 13:15:35 +0100

    trophys working

--- Assets/Scripts/loadTrophy.cs
+++ Assets/Scripts/loadTrophy.cs
@@ -10,16 +10,16 @@ public class loadTrophy : MonoBehaviour
     // Start is called before the first frame update
 
     public int[] streakCount = new int[] { 1, 3, 7, 10, 20, 50 };
-    public int[] trainingCount = new int[] { 1, 20, 50, 100}; //berrechnet mit allen Trainingsleveln
+    public int[] trainingCount = new int[] { 10, 30, 50, 100}; //berrechnet mit allen Trainingsleveln
     public int[] xpEarned = new int[] { 100, 500, 1000 }; // pro level 100 also LVL 2 - LVL 6 - LVL 11  Level-1 *100
 
 
-    public string streakGoal = "Aufstieg bei Streak = ";
-    public string trainingGoal = "Aufstieg nach ";
-    public string trainingGoal2 = " absolvierten Trainings";
+    private string streakGoal = "Aufstieg bei Streak > ";
+    private string trainingGoal = "Aufstieg nach ";
+    private string trainingGoal2 = " absolvierten Trainings";
 
-    public string xpGoal = "Aufstieg nach ";
-    public string xpGoal2 = " verdienten XP ";
+    private string xpGoal = "Aufstieg nach ";
+    private string xpGoal2 = " verdienten XP  ";
 
 
     public Texture medal1;
@@ -106,28 +106,34 @@ public class loadTrophy : MonoBehaviour
     private void checkTrain() {
 
 
-        int sessions = (playerData.levelExc1 + playerData.levelExc2 + playerData.levelExc3 + playerData.levelExc4 + playerData.levelExc5) - 5; // Abzug von 5, da Anfangslevel 1 für alle ist
+        int sessions = (playerData.levelExc1 + playerData.levelExc2 + playerData.levelExc3 + playerData.levelExc4 + playerData.levelExc5); 
+        Debug.Log(sessions + "sessions");
+        sessions = sessions - 5; // Abzug von 5, da Anfangslevel 1 für alle ist
+        Debug.Log(sessions + "sessions abzug");
+        
 
-        if (sessions < 1)
+        if (sessions < 10)
         {
             streakImage.texture = (Texture)noMedal; // Texture sessions 0
-            trainingCountTXT.text = trainingGoal + Convert.ToString(1)+ trainingGoal2;
+            trainingCountTXT.text = trainingGoal + Convert.ToString(10)+ trainingGoal2;
         }
 
-        else if (sessions == 1 )
+        else if (sessions <= 30)
         {
-            trainingImage.texture = (Texture)medal3; // Texture sessions 1
-            trainingCountTXT.text = trainingGoal + Convert.ToString(20) + trainingGoal2;
+            trainingImage.texture = (Texture)medal3; // Texture sessions 10
+            Debug.Log("session 10-30");
+            trainingCountTXT.text = trainingGoal + Convert.ToString(30) + trainingGoal2;
         }
-        else if (sessions > 1 && sessions < 20)
+        else if (sessions <= 50)
         {
-            trainingImage.texture = (Texture)medal2; // Texture sessions  2 - 20
+            trainingImage.texture = (Texture)medal2; // Texture sessions  30 -50
             trainingCountTXT.text = trainingGoal + Convert.ToString(50) + trainingGoal2;
         }
-        else if (sessions > 20)
+        else if (sessions > 50)
         {
-            trainingImage.texture = (Texture)medal1; // Texture sessions  < 20
+            trainingImage.texture = (Texture)medal1; // Texture sessions  < 50
             trainingCountTXT.text = trainingGoal + Convert.ToString(100)+ trainingGoal2;
+            Debug.Log("session 50+ " + sessions );
         }
 
     }
@@ -151,6 +157,7 @@ public class loadTrophy : MonoBehaviour
         else if (XP >= 500 && XP < 1000)
         {
             XPImage.texture = (Texture)medal2; // Texture XP  >= 500 && < 1000
+            Debug.Log("XPIMAGE CHANGE");
             XPGoalTXT.text = xpGoal + Convert.ToString(1000) + xpGoal2;
         }
         else if (XP > 1000)
